6cc0116d12675b730f30f0baa2eeb524636d7cc4
ByteFather <bytefather@gmail.com>
2015-03-10 20:59:33 +0100

    Added functionalities for setting filters order / sequence. Added information where emails are moved.

--- filters.php
+++ filters.php
@@ -5,11 +5,10 @@
  *
  * Plugin that adds a new tab to the settings section to create client-side e-mail filtering.
  *
- * @version 2.1.7
+ * @version 2.2.0
  * @author Roberto Zarrelli <zarrelli@unimol.it> 
  * @author Chris Simon <info@decomputeur.nl> from version 2.1.3
- * @author Waldemar Chrapkowski (Gzom) <bytefather@gmail.com> from version 2.1.6
- *   
+ * @author ByteFather <bytefather@gmail.com> from version 2.1.6
  */
  
 class filters extends rcube_plugin{
@@ -33,6 +32,7 @@ class filters extends rcube_plugin{
         $this->register_action('plugin.filters', array($this, 'filters_init'));
         $this->register_action('plugin.filters-save', array($this, 'filters_save'));
         $this->register_action('plugin.filters-delete', array($this, 'filters_delete'));                 
+        $this->register_action('plugin.filters-order', array($this, 'filters_order'));                 
         $this->add_texts('localization/', array('filters','nosearchstring'));       
         $this->rc->output->add_label('filters');
         $this->include_script('filters.js');      
@@ -51,7 +51,7 @@ class filters extends rcube_plugin{
 		// does not consider the messages already in the trash
     if ($open_mbox == $this->rc->config->get('trash_mbox'))
 		  return;
-          		    
+	
     //load filters
     $arr_prefs = $this->rc->config->get('filters', array());
             
@@ -86,7 +86,7 @@ class filters extends rcube_plugin{
         }                          
         if (count($uids)){                    
 				  $imap->move_message($uids, $dfolder, $open_mbox);
-				  $this->rc->output->show_message(count($uids)." ".$this->gettext('msg_moved_by_rule'), 'confirmation');
+				  $this->rc->output->show_message(count($uids)." ".$this->gettext('msg_moved_by_rule').$dfolder, 'confirmation');
         }                                  
       }            
       // refresh      
@@ -95,7 +95,7 @@ class filters extends rcube_plugin{
 			$this->api->output->command('getunread');
 			$this->api->output->send();                      
     }
-            			
+
   }
   
   function filters_save(){
@@ -149,6 +149,48 @@ class filters extends rcube_plugin{
     rcmail_overwrite_action('plugin.filters');
     $this->rc->output->send('plugin');      
   }    
+  
+/**
+ * Function changes order of specified filter
+ * @author ByteFather 
+ */
+  function filters_order() {
+	  $user = $this->rc->user;
+
+    $this->register_handler('plugin.body', array($this, 'filters_form'));
+    $this->rc->output->set_pagetitle($this->gettext('filters'));
+    
+    if ( isset($_GET[filterid]) && (!empty($_GET[filterid]) || '0' === $_GET[filterid]) && isset($_GET[forder]) && !empty($_GET[forder]) ){
+		$aNewFilterOrder = array();
+		$filter_id = $_GET[filterid];      
+		$arr_prefs = $user->get_prefs();
+		
+		switch(strtolower($_GET[forder])) {
+			case 'top':
+				$aNewFilterOrder = $this->aMoveElemTop($arr_prefs['filters'], $filter_id);
+				break;
+			case 'up':
+				$aNewFilterOrder = $this->aSequentElemUp($arr_prefs['filters'], $filter_id);
+				break;
+			case 'down':
+				$aNewFilterOrder = $this->aSequentElemDown($arr_prefs['filters'], $filter_id);
+				break;
+			case 'bottom':
+				$aNewFilterOrder = $this->aMoveElemBottom($arr_prefs['filters'], $filter_id);
+				break;
+		}
+      
+		$arr_prefs['filters'] = $aNewFilterOrder;
+      
+      if ($user->save_prefs($arr_prefs))
+        $this->rc->output->command('display_message', $this->gettext('successfullmoved'.strtolower($_GET[forder])), 'confirmation');
+      else
+        $this->rc->output->command('display_message', $this->gettext('unsuccessfullmoved'.strtolower($_GET[forder])), 'error');
+    }
+    
+    rcmail_overwrite_action('plugin.filters');
+    $this->rc->output->send('plugin');      
+  }
 
   function filters_init(){    
     $this->rc->output->set_pagetitle($this->gettext('filters'));
@@ -214,7 +256,7 @@ class filters extends rcube_plugin{
     $arr_prefs = $user->get_prefs();                    
     $i = 1;
     $flag=false;
-    $table2 = new html_table(array('cols' => 2));
+    $table2 = new html_table(array('cols' => 6));
     
     //To prevent PHP Warning when no filter already set
     if(!empty($arr_prefs['filters'])) {
@@ -232,12 +274,23 @@ class filters extends rcube_plugin{
 	          		}
 	        	}
 	      	}
+	      	if(empty($folder_name)){
+				$folder_name = $saved_filter['destfolder'];
+			}
 	      	$messages = $saved_filter['messages'];  
 	                                 
 	      	$msg = $i." - ".$this->gettext('msg_if_field')." <b>".$this->gettext($saved_filter['whatfilter'])."</b> ".$this->gettext('msg_contains')." <b>".$saved_filter['searchstring']."</b> ".($saved_filter['casesensitive'] == '1' ? $this->gettext('msg_and_is')." <b>".$this->gettext('casesensitive')."</b> ": "").$this->gettext('msg_move_msg_in')." <b>".$folder_name."</b> "."(".$this->gettext('messagecount').": ".$this->gettext($saved_filter['messages']).")";        
 	      	$table2->add('title',$msg);        
 	      	$dlink = "<a href='./?_task=settings&_action=plugin.filters-delete&filterid=".$key."'>".$this->gettext('delete')."</a>";                
 	      	$table2->add('title',$dlink);
+	      	$topLink = "<a href='./?_task=settings&_action=plugin.filters-order&filterid=".$key."&forder=top'>".$this->gettext('MoveTop')."</a>";
+	      	$table2->add('title',$topLink);
+	      	$bottomLink = "<a href='./?_task=settings&_action=plugin.filters-order&filterid=".$key."&forder=up'>".$this->gettext('MoveUp')."</a>";
+	      	$table2->add('title',$bottomLink);
+	      	$bottomLink = "<a href='./?_task=settings&_action=plugin.filters-order&filterid=".$key."&forder=down'>".$this->gettext('MoveDown')."</a>";
+	      	$table2->add('title',$bottomLink);
+	      	$bottomLink = "<a href='./?_task=settings&_action=plugin.filters-order&filterid=".$key."&forder=bottom'>".$this->gettext('MoveBottom')."</a>";
+	      	$table2->add('title',$bottomLink);
 	      	$i++;      
     	} 
     }
@@ -402,9 +455,104 @@ class filters extends rcube_plugin{
         $new_arr['destfolder'] = $destfolder; 
         $new_arr['messages'] = $messages;     
         $arr_prefs = $user->get_prefs();                    
-        $arr_prefs['filters'][] = $new_arr;
+        if(isset($arr_prefs['filters']) && is_array($arr_prefs['filters'])) {
+			$arr_prefs['filters'][] = $new_arr;
+		} else {
+			$arr_prefs['filters'] = array();
+			$arr_prefs['filters'][] = $new_arr;
+		}
         $user->save_prefs($arr_prefs);            
       }                          
   }
-      
+
+/**
+ * Function moves specified element on/to top of the given array
+ * @param array $aArray - source array to work on
+ * @param string/integer $iElemKey - key value of element to move on top
+ * @return new array
+ * @author ByteFather
+ */
+function aMoveElemTop($aSource, $iElemKey){
+	$aNewOrder = array();
+	
+	$aNewOrder[] = $aSource[$iElemKey];
+	unset($aSource[$iElemKey]);
+	
+	foreach($aSource as $key => $value){
+		$aNewOrder[] = $value;
+	}
+	
+	return $aNewOrder;
+}
+
+/**
+ * Function moves specified element to bottom of the given array
+ * @param array $aArray - source array to work on
+ * @param string/integer $iElemKey - key value of element to move at bottom
+ * @return new array
+ * @author ByteFather
+ */
+function aMoveElemBottom($aSource, $iElemKey){
+	$aNewOrder = array();
+	$mixBottomVal = $aSource[$iElemKey];
+	unset($aSource[$iElemKey]);
+	
+	foreach($aSource as $value){
+		$aNewOrder[] = $value;
+	}
+	
+	// adding last element, in fact shift element to bottom
+	$aNewOrder[] = $mixBottomVal;
+	
+	return $aNewOrder;
+}
+
+/**
+ * Function moves specified element up in the sequential array by given number of steps
+ * @param array $aSource - source array to work on
+ * @param string/integer $iElemKey - key value of element to move up
+ * @return new array
+ * @author ByteFather
+ */
+function aSequentElemUp($aSource, $iElemKey){
+	$aNewOrder = array();
+	$iKeyDelta = $iElemKey - 1;
+	
+	if($iKeyDelta <= 0){
+		$aNewOrder = $this->aMoveElemTop($aSource, $iElemKey);
+	} else {
+		$aNewOrder = $aSource;
+		$item = $aNewOrder[$iElemKey];
+		$aNewOrder[$iElemKey] = $aNewOrder[$iKeyDelta];
+		$aNewOrder[$iKeyDelta] = $item;
+	}
+	
+	return $aNewOrder;
+}
+
+
+/**
+ * Function moves specified element down in the sequential array by given number of steps
+ * @param array $aSource - source array to work on
+ * @param string/integer $iElemKey - key value of element to move down
+ * @return new array
+ * @author ByteFather
+ */
+function aSequentElemDown($aSource, $iElemKey){
+	$aNewOrder = array();
+	$aSize = count($aSource);
+	$iKeyDelta = $iElemKey + 1;
+	
+	if($iKeyDelta >= $aSize-1){ // -1 cos arrays are counted from 0
+		$aNewOrder = $this->aMoveElemBottom($aSource, $iElemKey);
+	} else {
+		$aNewOrder = $aSource;
+		$item = $aNewOrder[$iElemKey];
+		$aNewOrder[$iElemKey] = $aNewOrder[$iKeyDelta];
+		$aNewOrder[$iKeyDelta] = $item;
+	}
+	
+	return $aNewOrder;
+}
+
 }
